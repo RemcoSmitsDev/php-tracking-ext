9c2589fe28409c712029a00bce93dd504f497281
Remco Smits <djsmits12@gmail.com>
2025-02-15 19:07:06 +0100

    Move to observe api

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use chrono::{DateTime, Utc};
+use ext_php_rs::ffi::ZEND_INTERNAL_FUNCTION;
+use ext_php_rs::flags::FunctionType;
 use ext_php_rs::types::Zval;
 use ext_php_rs::{prelude::*, zend::ExecuteData};
 use lazy_static::lazy_static;
@@ -11,19 +13,6 @@ use std::sync::{
 };
 use std::time::{Duration, Instant};
 
-type ZendExecuteFuncExternal = Option<unsafe extern "C" fn(*mut ExecuteData)>;
-type ZendExecuteFuncInternal =
-    Option<unsafe extern "C" fn(*mut ExecuteData, *mut ext_php_rs::ffi::zend_value)>;
-
-extern "C" {
-    static mut zend_execute_ex: ZendExecuteFuncExternal;
-    static mut zend_execute_internal: ZendExecuteFuncInternal;
-}
-
-static mut ORIGINAL_EXECUTE_EX: ZendExecuteFuncExternal = None;
-static mut ORIGINAL_EXECUTE_INTERNAL: ZendExecuteFuncInternal = None;
-
-// Static storage for execution times and original execute_ex
 lazy_static! {
     static ref FUNCTION_CALLS: Mutex<Vec<CallType>> = Mutex::new(Vec::new());
     static ref REQUEST_START_TIME: Mutex<Option<DateTime<Utc>>> = Mutex::new(None);
@@ -38,10 +27,10 @@ struct FunctionCall {
     line: Option<u32>,
     name: String,
     stack_id: u64,
-    timestamp: i64,
     internal: bool,
     duration: Duration,
     parent_stack_id: u64,
+    timestamp: DateTime<Utc>,
     filename: Option<String>,
     namespace: Option<String>,
     arguments: Vec<ArgumentType>,
@@ -53,11 +42,11 @@ struct MethodCall {
     name: String,
     internal: bool,
     stack_id: u64,
-    timestamp: i64,
     classname: String,
     duration: Duration,
     file: Option<String>,
     parent_stack_id: u64,
+    timestamp: DateTime<Utc>,
     namespace: Option<String>,
     arguments: Vec<ArgumentType>,
 }
@@ -110,238 +99,7 @@ unsafe fn get_function_arguments(execute_data: *mut ExecuteData) -> Vec<Argument
     args
 }
 
-/// Custom execute_ex function for tracking
-#[no_mangle]
-pub unsafe extern "C" fn custom_execute_external(execute_data: *mut ExecuteData) {
-    let stack_id = STACK_ID_COUNTER.fetch_add(1, Ordering::SeqCst);
-
-    let arguments = get_function_arguments(execute_data);
-
-    // Get parent stack ID and depth
-    let parent_stack_id = {
-        let mut call_stack = CALL_STACK.lock().unwrap();
-        let parent_id = if call_stack.is_empty() {
-            0
-        } else {
-            *call_stack.back().unwrap()
-        };
-        call_stack.push_back(stack_id);
-        parent_id
-    };
-
-    let start_time = Instant::now();
-
-    // Call original execute_ex
-    unsafe {
-        if let Some(original) = ORIGINAL_EXECUTE_EX {
-            original(execute_data);
-        }
-    }
-
-    let elapsed = start_time.elapsed();
-
-    {
-        let mut call_stack = CALL_STACK.lock().unwrap();
-        call_stack.pop_back();
-    }
-
-    if execute_data.is_null() {
-        return;
-    }
-
-    let Some(execute_data_ref) = (unsafe { execute_data.as_ref() }) else {
-        return;
-    };
-
-    let Some(func) = execute_data_ref.function() else {
-        return;
-    };
-
-    let Some(name_ptr) = func.internal_function.function_name.as_ref() else {
-        return;
-    };
-
-    let (file, line) = {
-        let op_array = func.op_array;
-        let filename = if !op_array.filename.is_null() {
-            Some(
-                CStr::from_ptr((*op_array.filename).val.as_ptr())
-                    .to_string_lossy()
-                    .into_owned(),
-            )
-        } else {
-            None
-        };
-
-        (filename, op_array.line_start)
-    };
-
-    let function_name = CStr::from_ptr(name_ptr.val.as_ptr())
-        .to_string_lossy()
-        .into_owned();
-
-    let scope = func
-        .internal_function
-        .scope
-        .as_ref()
-        .and_then(|scope| scope.name())
-        .map(|scope| scope.to_owned());
-
-    if let Some(scope) = scope {
-        let parts: Vec<&str> = scope.rsplitn(2, '\\').collect();
-        let (classname, namespace) = match parts.as_slice() {
-            [classname, namespace] => (classname.to_string(), Some(namespace.to_string())),
-            [classname] => (classname.to_string(), None),
-            _ => return,
-        };
-
-        {
-            let mut calls = FUNCTION_CALLS.lock().unwrap();
-            calls.push(CallType::Method(MethodCall {
-                file,
-                stack_id,
-                classname,
-                arguments,
-                namespace,
-                parent_stack_id,
-                internal: false,
-                line: Some(line),
-                duration: elapsed,
-                name: function_name,
-                timestamp: chrono::Utc::now().timestamp(),
-            }));
-        }
-    } else {
-        let parts: Vec<&str> = function_name.rsplitn(2, '\\').collect();
-        let (function_name, namespace) = match parts.as_slice() {
-            [function_name, namespace] => (function_name.to_string(), Some(namespace.to_string())),
-            [function_name] => (function_name.to_string(), None),
-            _ => return,
-        };
-
-        {
-            let mut calls = FUNCTION_CALLS.lock().unwrap();
-            calls.push(CallType::Function(FunctionCall {
-                filename: file,
-                stack_id,
-                namespace,
-                arguments,
-                parent_stack_id,
-                internal: false,
-                line: Some(line),
-                duration: elapsed,
-                name: function_name,
-                timestamp: chrono::Utc::now().timestamp(),
-            }));
-        }
-    }
-}
-
-#[no_mangle]
-extern "C" fn custom_execute_internal(
-    execute_data: *mut ExecuteData,
-    return_value: *mut ext_php_rs::ffi::zend_value,
-) {
-    let stack_id = STACK_ID_COUNTER.fetch_add(1, Ordering::SeqCst);
-
-    if execute_data.is_null() {
-        return;
-    }
-
-    let Some(execute_data_ref) = (unsafe { execute_data.as_ref() }) else {
-        return;
-    };
-
-    let Some(func) = execute_data_ref.function() else {
-        return;
-    };
-
-    let Some(name_ptr) = (unsafe { func.internal_function.function_name.as_ref() }) else {
-        return;
-    };
-
-    let function_name = unsafe {
-        CStr::from_ptr(name_ptr.val.as_ptr())
-            .to_string_lossy()
-            .into_owned()
-    };
-
-    let arguments = unsafe { get_function_arguments(execute_data) };
-
-    let scope = unsafe { func.internal_function.scope.as_ref() }
-        .and_then(|scope| scope.name())
-        .map(|scope| scope.to_owned());
-
-    // Get parent stack ID
-    let parent_stack_id = {
-        let mut call_stack = CALL_STACK.lock().unwrap();
-        let parent_id = if call_stack.is_empty() {
-            0
-        } else {
-            *call_stack.back().unwrap()
-        };
-        call_stack.push_back(stack_id);
-        parent_id
-    };
-
-    let start_time = Instant::now();
-
-    // Call original function
-    unsafe {
-        if let Some(original) = ORIGINAL_EXECUTE_INTERNAL {
-            original(execute_data, return_value);
-        }
-    }
-
-    let elapsed = start_time.elapsed();
-    {
-        let mut call_stack = CALL_STACK.lock().unwrap();
-        call_stack.pop_back();
-    }
-
-    if let Some(scope) = scope {
-        let parts: Vec<&str> = scope.rsplitn(2, '\\').collect();
-        let (classname, namespace) = match parts.as_slice() {
-            [classname, namespace] => (classname.to_string(), Some(namespace.to_string())),
-            [classname] => (classname.to_string(), None),
-            _ => return,
-        };
-
-        {
-            let mut calls = FUNCTION_CALLS.lock().unwrap();
-            calls.push(CallType::Method(MethodCall {
-                stack_id,
-                classname,
-                namespace,
-                arguments,
-                file: None,
-                line: None,
-                internal: true,
-                parent_stack_id,
-                duration: elapsed,
-                name: function_name,
-                timestamp: chrono::Utc::now().timestamp(),
-            }));
-        }
-    } else {
-        {
-            let mut calls = FUNCTION_CALLS.lock().unwrap();
-            calls.push(CallType::Function(FunctionCall {
-                stack_id,
-                arguments,
-                line: None,
-                filename: None,
-                internal: true,
-                namespace: None,
-                parent_stack_id,
-                duration: elapsed,
-                name: function_name,
-                timestamp: chrono::Utc::now().timestamp(),
-            }));
-        }
-    }
-}
-
+#[cfg(debug_assertions)]
 fn print_call_tree(calls: &[CallType], parent_id: u64, level: usize) {
     for call in calls.iter().filter(|c| match c {
         CallType::Function(c) => c.parent_stack_id == parent_id,
@@ -417,6 +175,15 @@ pub extern "C" fn request_shutdown(_type: i32, _module: i32) -> i32 {
     // Print call tree
     let calls = FUNCTION_CALLS.lock().unwrap();
 
+    // Calculate total time in functions
+    let duration: Duration = calls
+        .iter()
+        .map(|call| match call {
+            CallType::Function(call) => call.duration,
+            CallType::Method(call) => call.duration,
+        })
+        .sum();
+
     let client = blocking::Client::builder()
         .timeout(Duration::from_secs(2))
         .build()
@@ -427,19 +194,23 @@ pub extern "C" fn request_shutdown(_type: i32, _module: i32) -> i32 {
         start_timestamp: DateTime<Utc>,
         end_timestamp: DateTime<Utc>,
         callstack: Vec<CallType>,
+        duration: Duration,
         url: Option<String>,
     }
 
-    client
+    let response = client
         .post("http://localhost:3000/requests")
         .json(&CreateRequest {
             start_timestamp,
             end_timestamp,
+            duration,
             callstack: calls.clone(),
             url: Some("https://my-application.nl/api/v1/users".to_owned()),
         })
         .send()
         .unwrap();
+
+    dbg!(response.status(), response.text());
 
     // if let Ok(mut file) = std::fs::OpenOptions::new()
     //     .write(true)
@@ -450,43 +221,167 @@ pub extern "C" fn request_shutdown(_type: i32, _module: i32) -> i32 {
     //     serde_json::to_writer_pretty(&mut file, &calls.clone()).unwrap();
     // }
 
-    // if cfg!(debug_assertions) {
-    //     println!("\nFunction call tree:");
-    //     print_call_tree(&calls, 0, 0);
-    // }
+    if cfg!(debug_assertions) {
+        println!("\nFunction call tree:");
+        print_call_tree(&calls, 0, 0);
+    }
 
-    // Calculate total time in functions
-    let total_function_time: Duration = calls
-        .iter()
-        .map(|call| match call {
-            CallType::Function(call) => call.duration,
-            CallType::Method(call) => call.duration,
-        })
-        .sum();
     drop(calls);
 
-    println!(
-        "\nTotal time in functions: {:?} ({:.2}% of request)",
-        total_function_time,
-        (total_function_time.as_nanos() as f64 / duration.num_nanoseconds().unwrap() as f64)
-            * 100.0
-    );
+    println!("\nTotal time in functions: {:?} ", duration);
 
     0
+}
+
+//
+// observer
+//
+
+static ACTIVE_CALLS: Mutex<VecDeque<(StackId, DateTime<Utc>, Instant)>> =
+    Mutex::new(VecDeque::new());
+
+// Define function pointer types for Zend observer handlers
+type ZendObserverFcallBeginHandler = extern "C" fn(*mut ExecuteData);
+type ZendObserverFcallEndHandler =
+    extern "C" fn(*mut ExecuteData, *mut ext_php_rs::ffi::zend_value);
+
+#[repr(C)]
+#[allow(non_camel_case_types)]
+pub struct zend_observer_fcall_handlers {
+    pub begin: Option<ZendObserverFcallBeginHandler>,
+    pub end: Option<ZendObserverFcallEndHandler>,
+}
+
+// External function bindings for the Zend API
+extern "C" {
+    pub fn zend_observer_fcall_register(
+        callback: extern "C" fn(*mut ExecuteData) -> zend_observer_fcall_handlers,
+    );
+}
+
+#[no_mangle]
+extern "C" fn observer_begin(_: *mut ExecuteData) {
+    if let Ok(mut active_calls) = ACTIVE_CALLS.lock() {
+        active_calls.push_back((
+            STACK_ID_COUNTER.fetch_add(1, Ordering::SeqCst),
+            chrono::Utc::now(),
+            Instant::now(),
+        ));
+    }
+}
+
+#[no_mangle]
+extern "C" fn observer_end(execute_data: *mut ExecuteData, _: *mut ext_php_rs::ffi::zend_value) {
+    let Ok(Some((stack_id, start_time, instant))) =
+        ACTIVE_CALLS.lock().map(|mut calls| calls.pop_back())
+    else {
+        return;
+    };
+
+    let elapsed = instant.elapsed();
+
+    let Some(execute_data_ref) = (unsafe { execute_data.as_ref() }) else {
+        return;
+    };
+
+    let Some(func) = execute_data_ref.function() else {
+        return;
+    };
+
+    let Some(name_ptr) = (unsafe { func.internal_function.function_name.as_ref() }) else {
+        return;
+    };
+
+    let function_name = unsafe {
+        CStr::from_ptr(name_ptr.val.as_ptr())
+            .to_string_lossy()
+            .into_owned()
+    };
+
+    let arguments = unsafe { get_function_arguments(execute_data) };
+
+    let scope = unsafe { func.internal_function.scope.as_ref() }
+        .and_then(|scope| scope.name())
+        .map(|scope| scope.to_owned());
+
+    // Get parent stack ID
+    let parent_stack_id = {
+        let mut call_stack = CALL_STACK.lock().unwrap();
+        let parent_id = if call_stack.is_empty() {
+            0
+        } else {
+            *call_stack.back().unwrap()
+        };
+        call_stack.push_back(stack_id);
+        parent_id
+    };
+
+    if let Ok(mut call_stack) = CALL_STACK.lock() {
+        call_stack.pop_back();
+    }
+
+    if let Some(scope) = scope {
+        let parts: Vec<&str> = scope.rsplitn(2, '\\').collect();
+        let (classname, namespace) = match parts.as_slice() {
+            [classname, namespace] => (classname.to_string(), Some(namespace.to_string())),
+            [classname] => (classname.to_string(), None),
+            _ => return,
+        };
+
+        {
+            let mut calls = FUNCTION_CALLS.lock().unwrap();
+            calls.push(CallType::Method(MethodCall {
+                stack_id,
+                classname,
+                namespace,
+                arguments,
+                file: None,
+                line: None,
+                parent_stack_id,
+                duration: elapsed,
+                name: function_name,
+                timestamp: start_time,
+                internal: unsafe { func.type_ } as u32 == ZEND_INTERNAL_FUNCTION,
+            }));
+        }
+    } else {
+        {
+            let mut calls = FUNCTION_CALLS.lock().unwrap();
+            calls.push(CallType::Function(FunctionCall {
+                stack_id,
+                arguments,
+                line: None,
+                filename: None,
+                namespace: None,
+                parent_stack_id,
+                duration: elapsed,
+                name: function_name,
+                timestamp: start_time,
+                internal: unsafe { func.type_ } as u32 == ZEND_INTERNAL_FUNCTION,
+            }));
+        }
+    }
+}
+
+#[no_mangle]
+extern "C" fn observer_callback(_execute_data: *mut ExecuteData) -> zend_observer_fcall_handlers {
+    zend_observer_fcall_handlers {
+        begin: Some(observer_begin),
+        end: Some(observer_end),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn register_observer() {
+    unsafe {
+        zend_observer_fcall_register(observer_callback);
+    }
 }
 
 /// Initialize the module and setup function tracking
 #[php_module]
 pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
-    unsafe {
-        // Store original execute_ex and execute_internal
-        ORIGINAL_EXECUTE_EX = zend_execute_ex;
-        ORIGINAL_EXECUTE_INTERNAL = zend_execute_internal;
-
-        // Override with our custom functions
-        zend_execute_ex = Some(custom_execute_external);
-        zend_execute_internal = Some(custom_execute_internal);
-    }
+    register_observer();
 
     module
         .request_startup_function(request_startup)
